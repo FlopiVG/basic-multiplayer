5a7753d8c9f19c37b3df1308f44082c5ce14df08
Slava Bezgachev <bezgachev@gmail.com>
2018-10-06 00:21:20 +0600

    Re-arranged code a little, a simple test works now

--- server/__tests__/index.test.ts
+++ server/__tests__/index.test.ts
@@ -1,9 +1,27 @@
 import "jest";
+import { Application } from "express";
+import supertest = require("supertest");
 
-import { App } from "../index";
+import { App } from "../App";
+
+let app: Application;
+let request: supertest.SuperTest<supertest.Test>;
+
 
 describe("Server test suite", () => {
-  test("The app starts without errors", () => {
-    new App();
+
+  beforeAll(() => {
+    app = new App().app;
+  });
+
+  test("The app starts without errors", async () => {
+    request = supertest(app);
+    const response = await request.get("/");
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("text/html");
+  });
+
+  afterAll((done) => {
+    app.close(done);
   });
 });
